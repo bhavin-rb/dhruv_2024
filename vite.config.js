899ac0b8a460e3010888df9bf58c7248795db649
Bhavin Borkhataria <bhavin1234@gmail.com>
2023-12-31 13:49:01 +0300

    update file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/druv_2024/',
+  base: '/dhruv_2024/',
   plugins: [react()],
 })
